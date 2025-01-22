49b1ae5ed35359131a768ec979c850348f55ea47
amanbora856 <amanbora30172@gmail.com>
2025-01-22 19:50:56 +0530

    Changed the linkedin url

--- src/portfolio.js
+++ src/portfolio.js
@@ -4,7 +4,7 @@
 // Summary And Greeting Section
 
 const illustration = {
-  animated: true // Set to false to use static SVG
+  animated: true, // Set to false to use static SVG
 };
 
 const greeting = {
@@ -12,10 +12,9 @@ const greeting = {
   title: "Hi all, I'm Aman",
   subTitle:
     "A results-driven Software Developer with 3+ years of experience in delivering efficient software solutions and optimizing infrastructure costs. Expertise in Node.js, TypeScript, Go, DevOps, and cloud technologies. Adept at working with cross-functional teams, mentoring junior developers, and deploying scalable Web3 and Web2 applications. Proven success in reducing billing costs by 92% through strategic API optimizations and enhancing system performance by 30% using agile methodologies. Strong focus on automating processes and improving operational efficiency.",
-  resumeLink:
-    "https://docs.google.com/document/d/1IYI83e8_o7wWPAemuaewSqBIDtStwapJeolsr46WGL0/export?format=pdf", // Set to empty to hide the button
+  resumeLink: "https://docs.google.com/document/d/1IYI83e8_o7wWPAemuaewSqBIDtStwapJeolsr46WGL0/export?format=pdf", // Set to empty to hide the button
   resumeName: "Aman's Resume.pdf",
-  displayGreeting: true // Set false to hide this section, defaults to true
+  displayGreeting: true, // Set false to hide this section, defaults to true
 };
 
 // Social Media Links
@@ -23,7 +22,7 @@ const greeting = {
 const socialMediaLinks = {
   github: "https://github.com/amanbora856",
   gitlab: "https://gitlab.com/amanbora856",
-  linkedin: "www.linkedin.com/in/aman-bora-dev",
+  linkedin: "https://www.linkedin.com/in/aman-bora-dev/",
   gmail: "amanbora30172@gmail.com",
   medium: "https://medium.com/@amanbora30172",
   npm: "https://www.npmjs.com/~aman_bytemaster",
@@ -31,7 +30,7 @@ const socialMediaLinks = {
   twitter: "https://twitter.com/AmanBor11568380",
   discord: "https://discordapp.com/users/aman_beast",
   // To customize icons and social links, tweak src/components/SocialMedia
-  display: true // Set true to display this section, defaults to false
+  display: true, // Set true to display this section, defaults to false
 };
 
 // Skills Section
@@ -43,7 +42,7 @@ const skillsSection = {
     "⚡️ Expertise in developing and delivering solutions, leveraging traditional technologies for optimal performance.",
     "⚡️ Skilled in cost optimization strategies, reducing Google API billing by 92% and Azure egress data transfer costs by 75%.",
     "⚡️ Proficient in mentoring and upskilling teams, improving proficiency in frameworks like Express Js",
-    "⚡️ Strong ability to streamline processes through automation, achieving significant reductions in manual interventions and operational overhead."
+    "⚡️ Strong ability to streamline processes through automation, achieving significant reductions in manual interventions and operational overhead.",
   ],
 
   /* Make Sure to include correct Font Awesome Classname to view your icon
@@ -52,70 +51,70 @@ https://fontawesome.com/icons?d=gallery */
   softwareSkills: [
     {
       skillName: "JavaScript",
-      fontAwesomeClassname: "fab fa-js"
+      fontAwesomeClassname: "fab fa-js",
     },
     {
       skillName: "Golang",
-      fontAwesomeClassname: "fas fa-cogs"
+      fontAwesomeClassname: "fas fa-cogs",
     },
     {
       skillName: "Shell",
-      fontAwesomeClassname: "fab fa-linux"
+      fontAwesomeClassname: "fab fa-linux",
     },
     {
       skillName: "Reactjs",
-      fontAwesomeClassname: "fab fa-react"
+      fontAwesomeClassname: "fab fa-react",
     },
     {
       skillName: "Nodejs",
-      fontAwesomeClassname: "fab fa-node"
+      fontAwesomeClassname: "fab fa-node",
     },
     {
       skillName: "npm",
-      fontAwesomeClassname: "fab fa-npm"
+      fontAwesomeClassname: "fab fa-npm",
     },
     {
       skillName: "Mongo DB",
-      fontAwesomeClassname: "fab fa-envira"
+      fontAwesomeClassname: "fab fa-envira",
     },
     {
       skillName: "AWS",
-      fontAwesomeClassname: "fab fa-aws"
+      fontAwesomeClassname: "fab fa-aws",
     },
     {
       skillName: "Digital Ocean",
-      fontAwesomeClassname: "fab fa-digital-ocean"
+      fontAwesomeClassname: "fab fa-digital-ocean",
     },
     {
       skillName: "Docker",
-      fontAwesomeClassname: "fab fa-docker"
+      fontAwesomeClassname: "fab fa-docker",
     },
     {
       skillName: "Git",
-      fontAwesomeClassname: "fab fa-git"
+      fontAwesomeClassname: "fab fa-git",
     },
     {
       skillName: "Kubernetes",
-      fontAwesomeClassname: "fas fa-network-wired"
+      fontAwesomeClassname: "fas fa-network-wired",
     },
     {
       skillName: "TypeScript",
-      fontAwesomeClassname: "fab fa-js-square"
+      fontAwesomeClassname: "fab fa-js-square",
     },
     {
       skillName: "Message Queue",
-      fontAwesomeClassname: "fas fa-exchange-alt"
+      fontAwesomeClassname: "fas fa-exchange-alt",
     },
     {
       skillName: "Postman",
-      fontAwesomeClassname: "fas fa-flask"
+      fontAwesomeClassname: "fas fa-flask",
     },
     {
       skillName: "Networking",
-      fontAwesomeClassname: "fas fa-project-diagram"
-    }
+      fontAwesomeClassname: "fas fa-project-diagram",
+    },
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: true, // Set false to hide this section, defaults to true
 };
 
 // Education Section
@@ -129,10 +128,7 @@ const educationInfo = {
       subHeader: "Bachelor of Engineering in Information & Technology",
       duration: "August 2016 - June 2020",
       desc: "Took extra courses such as  Operating System, Machine Design ...",
-      descBullets: [
-        "CGPA: 8.02",
-        "Participated in various workshops such as AI, ML, MachineDesign, ..."
-      ]
+      descBullets: ["CGPA: 8.02", "Participated in various workshops such as AI, ML, MachineDesign, ..."],
     },
     {
       schoolName: "Rani Laxmi Bai Memorial School",
@@ -140,12 +136,9 @@ const educationInfo = {
       subHeader: "InterMediate Education ",
       duration: "September 2014 - March 2016",
       desc: "Took Active Interest in the coding contests..",
-      descBullets: [
-        "Percentage: 79%",
-        "Major: Physics, Chemistry, Mathematics and Computers"
-      ]
-    }
-  ]
+      descBullets: ["Percentage: 79%", "Major: Physics, Chemistry, Mathematics and Computers"],
+    },
+  ],
 };
 
 // Your top 3 proficient stacks/tech experience
@@ -155,18 +148,18 @@ const techStack = {
   experience: [
     {
       Stack: "Programming",
-      progressPercentage: "80%"
+      progressPercentage: "80%",
     },
     {
       Stack: "Backend",
-      progressPercentage: "69%"
+      progressPercentage: "69%",
     },
     {
       Stack: "Devops", //Insert stack or technology you have experience in
-      progressPercentage: "58%" //Insert relative proficiency in percentage
-    }
+      progressPercentage: "58%", //Insert relative proficiency in percentage
+    },
   ],
-  displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+  displayCodersrank: false, // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
 };
 
 // Work experience section
@@ -188,14 +181,14 @@ const workExperiences = {
         "Optimized Google API calls, implementing efficient request strategies that resulted in a 92% reduction in billing costs while maintaining performance and functionality.",
         "Achieved a 75% reduction in Azure egress data transfer costs through strategic optimizations, significantly lowering operational expenses.",
         "Led a cross-functional team of developers to deliver a comprehensive software solution, improving system efficiency by 30%, utilizing Agile methodologies and technologies such as TypeScript, Solidity, and MongoDB.",
-        "Demonstrated adaptability and a quick learning curve, consistently delivering high-quality code that met or exceeded client expectations."
+        "Demonstrated adaptability and a quick learning curve, consistently delivering high-quality code that met or exceeded client expectations.",
       ],
       footerLink: [
         {
           name: "cisin.com",
-          url: "https://www.cisin.com/"
-        }
-      ]
+          url: "https://www.cisin.com/",
+        },
+      ],
     },
     {
       role: "Blockchain Developer",
@@ -208,14 +201,14 @@ const workExperiences = {
         "At Qodequay, I developed innovative features using Web3 and Web2 technologies to create scalable applications.",
         "I leveraged my expertise in Object - Oriented Programming(OOP), JavaScript, ES6, and TypeScript to write maintainable code and build dynamic, responsive applications.",
         "I designed user - friendly, authenticated apps and worked on developing REST APIs, adopting new technologies to efficiently solve complex problems.",
-        "I utilized real - time communication protocols like socket.io and MQTT to create interactive applications with live data and messaging features."
+        "I utilized real - time communication protocols like socket.io and MQTT to create interactive applications with live data and messaging features.",
       ],
       footerLink: [
         {
           name: "Qodequay.com",
-          url: "https://www.qodequay.com/"
-        }
-      ]
+          url: "https://www.qodequay.com/",
+        },
+      ],
     },
     {
       role: "Backend Developer",
@@ -229,16 +222,16 @@ const workExperiences = {
         "Worked on the authorization process to ensure secure user access.",
         "Developed scripts for uploading product data efficiently.",
         "Conducted thorough regression testing to identify and resolve functionality issues.",
-        "Ensured optimal performance and improved user satisfaction through testing and issue resolution."
+        "Ensured optimal performance and improved user satisfaction through testing and issue resolution.",
       ],
-       footerLink: [
+      footerLink: [
         {
-         name: "Wyse Nybble",
-         //url: "https://www.qodequay.com/"
-        }
-       ]
-    }
-  ]
+          name: "Wyse Nybble",
+          //url: "https://www.qodequay.com/"
+        },
+      ],
+    },
+  ],
 };
 
 /* Your Open Source Section to View Your Github Pinned Projects
@@ -246,7 +239,7 @@ To know how to get github key look at readme.md */
 
 const openSource = {
   showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
-  display: false // Set false to hide this section, defaults to true
+  display: false, // Set false to hide this section, defaults to true
 };
 
 // Some big projects you have worked on
@@ -265,9 +258,9 @@ const bigProjects = {
         //  you can add extra buttons here.
         {
           name: "OET Website",
-          url: "https://www.oet.com"
-        }
-      ]
+          url: "https://www.oet.com",
+        },
+      ],
     },
     {
       image: require("./assets/images/oetLogo.png"),
@@ -279,20 +272,19 @@ const bigProjects = {
         //  you can add extra buttons here.
         {
           name: "OET Website",
-          url: "https://www.oet.com"
-        }
-      ]
+          url: "https://www.oet.com",
+        },
+      ],
     },
 
     {
       image: require("./assets/images/edtech-min.jpg"),
       projectName: "Ed-Tech",
       date: "April 2023 - June2023",
-      projectDesc:
-        "I worked with the development team to create a PaaS solution for rural education districts, focusing on backend development to enhance accessibility and streamline processes.",
+      projectDesc: "I worked with the development team to create a PaaS solution for rural education districts, focusing on backend development to enhance accessibility and streamline processes.",
       footerLink: [
         //  you can add extra buttons here.
-      ]
+      ],
     },
     {
       image: require("./assets/images/nft-min.jpg"),
@@ -302,7 +294,7 @@ const bigProjects = {
         "A white-labeled NFT marketplace was developed on the Ethereum blockchain, allowing users to mint, sell, and buy NFTs. The platform utilized Solidity for smart contract development, Node.js for the backend, and MongoDB for database management. This customizable, decentralized marketplace ensured secure transactions and ownership verification through blockchain technology.",
       footerLink: [
         //  you can add extra buttons here
-      ]
+      ],
     },
     {
       image: require("./assets/images/hp_protrain.jpg"),
@@ -314,12 +306,12 @@ const bigProjects = {
         //  you can add extra buttons here.
         {
           name: "Hp Protrain",
-          url: "https://hpprotrain.com/"
-        }
-      ]
+          url: "https://hpprotrain.com/",
+        },
+      ],
     },
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: true, // Set false to hide this section, defaults to true
 };
 
 // Achievement Section
@@ -327,8 +319,7 @@ const bigProjects = {
 
 const achievementSection = {
   title: "Achievements And Certifications 🏆 ",
-  subtitle:
-    "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+  subtitle: "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
 
   achievementsCards: [
     {
@@ -338,47 +329,33 @@ const achievementSection = {
       footerLink: [
         {
           name: "Certification",
-          url: "https://www.blockchain-council.org/certifications/certified-blockchain-developer/"
-        }
-      ]
+          url: "https://www.blockchain-council.org/certifications/certified-blockchain-developer/",
+        },
+      ],
     },
     {
       title: "Machine Learning and AI workshop",
-      subtitle:
-        "Participated on the Ai workshop that was hosted by the IIT Roorkee on March 2019",
+      subtitle: "Participated on the Ai workshop that was hosted by the IIT Roorkee on March 2019",
       image: require("./assets/images/AiWorkshop.jpg"),
       footerLink: [
         {
           name: "Certification",
-          url: ""
+          url: "",
         },
         {
           name: "Participation",
-          url: ""
-        }
-      ]
-    }
+          url: "",
+        },
+      ],
+    },
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: true, // Set false to hide this section, defaults to true
 };
 
 const contactInfo = {
   title: "Contact Me ☎️",
-  subtitle:
-    "Discuss a project or just want to say hi? My Inbox is open for all.",
-  email_address: "amanbora30172@gmail.com"
+  subtitle: "Discuss a project or just want to say hi? My Inbox is open for all.",
+  email_address: "amanbora30172@gmail.com",
 };
 
-export {
-  achievementSection,
-  bigProjects,
-  contactInfo,
-  educationInfo,
-  greeting,
-  illustration,
-  openSource,
-  skillsSection,
-  socialMediaLinks,
-  techStack,
-  workExperiences
-};
+export { achievementSection, bigProjects, contactInfo, educationInfo, greeting, illustration, openSource, skillsSection, socialMediaLinks, techStack, workExperiences };
